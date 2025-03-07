2138b798adf8cde39a99856eecab557e4d447bbf
TimSensson <tim.svensson2@elev.ga.ntig.se>
2025-03-07 10:01:53 +0100

    Added about

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.route("/about")
+def about():
+    return "<p>We are the best!</p>"
+
 if __name__ == "__main__":
     app.run()
